c86445fdcfe29bdfddc155deffda0c8e2ee07600
Fábio Previatti <fpreviatti@hotmail.com>
2026-03-15 14:41:34 -0300

    Inclui chamada do get para retornar os beneficios

--- backend-module/src/main/java/com/example/backend/controller/BeneficioController.java
+++ backend-module/src/main/java/com/example/backend/controller/BeneficioController.java
@@ -2,13 +2,16 @@ package com.example.backend.controller;
 
 import com.example.backend.service.BeneficioService;
 
+import com.example.ejb.entity.Beneficio;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/beneficios")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BeneficioController {
 
     private final BeneficioService beneficioService;
@@ -25,4 +28,11 @@ public class BeneficioController {
         beneficioService.transfer(fromId, toId, amount);
         return ResponseEntity.ok("Transferência realizada");
     }
+
+    @GetMapping
+    public ResponseEntity<List<Beneficio>> getBeneficios() {
+        List<Beneficio> beneficios = beneficioService.findAll();
+        return ResponseEntity.ok(beneficios);
+    }
+
 }
