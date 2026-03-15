cabb65f982e079fa4338444091b181837d49f61e
Fábio Previatti <fpreviatti@hotmail.com>
2026-03-14 23:28:58 -0300

    Ajusta as configuracoes e compila o projeto

--- ejb-module/src/main/java/com/example/ejb/BeneficioEjbService.java
+++ ejb-module/src/main/java/com/example/ejb/BeneficioEjbService.java
@@ -12,14 +12,14 @@ public class BeneficioEjbService {
     private EntityManager em;
 
     public void transfer(Long fromId, Long toId, BigDecimal amount) {
-        Beneficio from = em.find(Beneficio.class, fromId);
-        Beneficio to   = em.find(Beneficio.class, toId);
-
-        // BUG: sem validações, sem locking, pode gerar saldo negativo e lost update
-        from.setValor(from.getValor().subtract(amount));
-        to.setValor(to.getValor().add(amount));
-
-        em.merge(from);
-        em.merge(to);
+//        Beneficio from = em.find(Beneficio.class, fromId);
+//        Beneficio to   = em.find(Beneficio.class, toId);
+//
+//        // BUG: sem validações, sem locking, pode gerar saldo negativo e lost update
+//        from.setValor(from.getValor().subtract(amount));
+//        to.setValor(to.getValor().add(amount));
+//
+//        em.merge(from);
+//        em.merge(to);
     }
 }
