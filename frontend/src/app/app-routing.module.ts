6c4375ccc7521c36c60e0a89174cb0ddd650b07b
Fábio Previatti <fpreviatti@hotmail.com>
2026-03-15 17:03:56 -0300

    Corrige rotas e remove componente nao utilizado

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { TransferenciaComponent } from './component/transferencia/transferencia.component';
+import { BeneficioComponent } from './component/beneficio/beneficio.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: 'transferencia', component: TransferenciaComponent },
+  { path: 'beneficio', component: BeneficioComponent },
   { path: '', redirectTo: 'transferencia', pathMatch: 'full' },
   { path: '**', redirectTo: 'transferencia', pathMatch: 'full' }
 ];
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,21 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, Routes } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { TransferenciaComponent } from './app/component/transferencia/transferencia.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'transferencia', pathMatch: 'full' },
-  { path: 'transferencia', component: TransferenciaComponent }
-];
+import { AppRoutingModule } from './app/app-routing.module';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes),
-    provideAnimations(),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule, AppRoutingModule),
+    provideAnimations()
   ]
-})
-  .catch(err => console.error(err));
+});
