6b16a82bc1f75ed8c89a3a3e1cef4a11e44501e7
Fábio Previatti <fpreviatti@hotmail.com>
2026-03-15 17:01:03 -0300

    Corrige o roteamento no frontend

---
+++ frontend/src/app/component/beneficio/beneficio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'beneficio',
+  imports: [],
+  templateUrl: './beneficio.component.html',
+  styleUrl: './beneficio.component.css'
+})
+export class BeneficioComponent {
+
+}
