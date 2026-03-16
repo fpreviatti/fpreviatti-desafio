7cfb2e453059a9a91d6c7d5f0a0ca98dc1d3db60
Fábio Previatti <fpreviatti@hotmail.com>
2026-03-15 23:46:47 -0300

    Remove imports nao utilizados e comentarios

--- backend-module/src/main/java/com/desafio/backend/repository/BeneficioRepository.java
+++ backend-module/src/main/java/com/desafio/backend/repository/BeneficioRepository.java
@@ -4,7 +4,5 @@ import com.desafio.ejb.entity.Beneficio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface BeneficioRepository extends JpaRepository<Beneficio, Long> {}
--- frontend/src/setup-jest.ts
+++ frontend/src/setup-jest.ts
@@ -1,13 +1,9 @@
-// src/setup-jest.ts
 try {
-  // usar require evita problemas de interoperabilidade ESM/CJS
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const { setupZoneTestEnv } = require('jest-preset-angular/setup-env/zone');
   setupZoneTestEnv();
 } catch (err: any) {
   const msg = err && err.message ? err.message : String(err);
   if (msg.includes('Cannot set base providers because it has already been called')) {
-    // ambiente já inicializado: ignorar
   } else {
     throw err;
   }
